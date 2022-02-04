d0ac6a6b273990a32f9552b14547d990441b2416
antomuto4 <56132390+antomuto4@users.noreply.github.com>
2022-02-04 22:31:57 +0100

    updated: added airport and colors for choices

--- text-adventure.c
+++ text-adventure.c
@@ -10,26 +10,28 @@ int r_level;
 int c_level;
 
 int rng;
-  
+
+void setup(){
+    srand(time(0));
+    r_level = rand() % 3;
+    rng = rand() % 7; 
+}
+
 int main(){
   printf("\t\033[0;32mHow many \033[0;33mlevels \033[0;31mcan you go through before \033[0;35mfate \033[0;36mchooses not to help you any longer?\n\n");
   printf("\033[0m");
-
+  
   while(1){
-    srand(time(0));
-    r_level = rand() % 2;
-    rng = rand() % 7;
+    setup();
 
-    printf("\nRNG: %d\nr_level: %d", rng, r_level);
-    
     switch(r_level){
     case 0:
-      printf("\n\nDo you want to move forward? (enter number)\n1) yes\n2) no\n");
+      printf("\n\nDo you want to move forward? (enter number)\n\x1B[32m1) yes\n\x1B[31m2) no\n\033[0m");
       scanf("%d", &prompt);
       if(prompt == 1){
 	printf("\nYou continued to walk..");
 	c_level++;
-	r_level = rand() % 2;
+	r_level = rand() % 3;
       }
       
       else if(rng == 0){
@@ -37,7 +39,7 @@ int main(){
 	printf("\nYou survived for: %d level(s)", c_level);
 	return 1;
       }
-
+      
       else{
 	health += 10;
 	printf("\nExhaustion was reduced. Health increased by 10. Your health is now at: %d", health);
@@ -45,31 +47,61 @@ int main(){
       
       break;
     case 1:
-      printf("\n\nThere is a lake in front of you, do you want to cross it?\nCrossing may decrease your health. Your health is: %i. (enter number)\n1) yes\n2) no\n", health);
+      printf("\n\nThere is a lake in front of you, do you want to cross it?\nCrossing may decrease your health. Your health is: %i. (enter number)\n\x1B[32m1) yes\n\x1B[31m2) no\n\033[0m", health);
       scanf("%d", &prompt);
       if(prompt == 1){
 	health -= 10;
 	printf("\nYou chose to swim through the lake. You lost 10 hp. Your health is now at: %i", health);
 	c_level++;
-	r_level = rand() % 2;
+	r_level = rand() % 3;
       }
-
+      
       else if(rng == 0){
 	printf("You rest for a while. But a zombie jumps out of the water and kills you.");
 	printf("\nYou survived for: %d level(s)", c_level);
 	return 1;
       }
-
+      
       else {
 	health += 5;
 	printf("\nYou stood still for a while and recovered 5 hp. Your health is now at: %d.", health);
       }
       break;
+
+    case 2:      
+      printf("\nYou found an airport. It's possible to find weapons in this area, but it's easy to get spotted. (enter number)\n\x1B[32m1) look around\n\x1B[31m2) forget it\n\033[0m");
+      scanf("%d", &prompt);
+
+      if(prompt == 1){
+	printf("\nYou scan the area..\nThere lies a security guard on the ground, will you search it? (enter number)\n\x1B[32m1) search body\n\x1B[31m2) nevermind, get the hell out of here\n\033[0m");
+	scanf("%d", &prompt);
+	
+	if(prompt == 1){
+	  if(rng == 0){
+	    printf("\nThe zombie woke up and bit you to death.\n");
+	    printf("\nYou survived for: %d level(s)", c_level);
+	    return 1;
+	  }
+	  
+	  else{
+	    printf("\nYou got \x1B[34m<item>\033[0m, this may come in use in the future.");
+	    c_level++;
+	    r_level = rand() % 3;
+	  }
+	}
+      }
+      
+      
+      else{
+	c_level++;
+	r_level = rand() % 3;
+      }
+	break;
     default:
       return 1;
       break;
     }
   }
-    
-    return 0;
-  }
+  
+  return 0;
+}
